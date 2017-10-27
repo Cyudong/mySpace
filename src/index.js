870a9f0d237e33911646968697fcdd5bc263da38
chenyudong <wb-chenyudong.b@alibaba-inc.com>
2017-10-27 18:00:04 +0800

    new file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// 这是项目的核心文件。全局的配置都在这个文件里面配置
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './app.vue'
+import router from './router.js'
+import './assets/less/index.less'
+
+Vue.config.debug = true; // 开启错误提示
+Vue.use(VueResource)
+Vue.http.options.emulateHTTP = true
+
+new Vue({
+  router,
+  el: '#appRoot',
+  render: h => h(App)
+})
